44ad28330aa903f553f350ea6259ed136f80da65
Clovin <wxxzhuang@gmail.com>
2018-01-05 03:49:58 +0800

    compress some file

--- public/annual2016/fullPage.js
+++ public/annual2016/fullPage.js
@@ -1,15 +1,1 @@
-/**
- * Created by XX on 2016/11/24.
- */
-$(document).ready(function () {
-    var full_img = $('.full-img img');
-    var wnd = $(window);
-
-    full_img.css('width', wnd.width());
-    full_img.css('height', wnd.height());
-
-    $(window).resize(function () {
-        full_img.css('width', wnd.width());
-        full_img.css('height', wnd.height());
-    })
-});
+$(document).ready(function(){var a=$(".full-img img");var b=$(window);a.css("width",b.width());a.css("height",b.height());$(window).resize(function(){a.css("width",b.width());a.css("height",b.height())})});
