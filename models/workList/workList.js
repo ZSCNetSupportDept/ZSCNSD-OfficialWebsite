2d373e4d9e7f8be88b317fdfb48f6829f926bbc0
Clovin <wxxzhuang@gmail.com>
2018-09-05 17:09:22 +0800

    feat: xlsx read rule

--- models/workList/workList.js
+++ models/workList/workList.js
@@ -2,83 +2,98 @@
  * Created by Clovin on 21/12/2017.
  */
 
-const XLSX = require('xlsx')
-const fs = require('fs')
-let filePath = ``
+const XLSX = require('xlsx');
+const fs = require('fs');
+let filePath = ``;
 
-let data = {}
+let data = {};
 
 function start (path) {
-  filePath = path
-  refreshData()
+    filePath = path;
+    refreshData();
 
-  fs.watchFile(filePath, () => {
-    refreshData()
-  })
+    fs.watchFile(filePath, () => {
+        refreshData();
+    });
 }
 
 function refreshData () {
-  let workbook = XLSX.readFile(filePath)
-  let firstSheetName = workbook.SheetNames[0]
-  let firstSheet = workbook.Sheets[firstSheetName]
+    let workbook = XLSX.readFile(filePath);
+    let firstSheetName = workbook.SheetNames[0];
+    let firstSheet = workbook.Sheets[firstSheetName];
 
-  let jsonSheet = XLSX.utils.sheet_to_json(firstSheet, {
-    header: ['地点', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
-  })
+    let jsonSheet = XLSX.utils.sheet_to_json(firstSheet, {
+        header: ['地点', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
+    });
 
-  // handle table data
-  let tableData = jsonSheet.filter(item => {
-    if (item['地点'] && Object.keys(item).length === 8) {
-      return item
-    }
-  })
-  let tempTable = [];
-  ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'].forEach(day => {
-    tempTable.push({
-      title: day,
-      content: []
-    })
-  })
-  tableData.forEach(item => {
-    ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'].forEach((day, index) => {
-      let data = item[day].split(/-+/)
+    // handle table data
+    let tableData = jsonSheet.filter(item => {
+        if (item['地点'] && Object.keys(item).length === 8) {
+            return item;
+        }
+    });
+    let tempTable = [];
+    ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'].forEach(day => {
+        tempTable.push({
+            title: day,
+            content: []
+        });
+    });
+    tableData.forEach(item => {
+        ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'].forEach((day, index) => {
+            let data = item[day].split(/ +/);
 
-      tempTable[index].content.push({
-        place: item['地点'],
-        worker: data[0],
-        phone: data[1] ? data[1] : '报修请联系组长'
-      })
-    })
-  })
+            if (item['地点'] !== '值日组长') {
+                tempTable[index].content.push({
+                    place: item['地点'],
+                    worker: data[0],
+                    phone: data[1] ? data[1] : '报修请联系组长'
+                });
+            }
+        });
+    });
 
-  // handle tousu data
-  let tempTousu = []
-  jsonSheet.forEach(item => {
-    for (let value of Object.values(item)) {
-      if (value.includes('投诉')) {
-        let temp = value.match(/\s+(\S+)\s+(\S+)/)
-        tempTousu.push({
-          worker: temp[1].split(/-+/)[0],
-          phone: temp[1].split(/-+/)[1]
-        })
-        tempTousu.push({
-          worker: temp[2].split(/-+/)[0],
-          phone: temp[2].split(/-+/)[1]
-        })
-        return
-      }
-    }
-  })
+    // handle leader data
+    const days = ['一', '二', '三', '四', '五', '六', '日'];
+    jsonSheet.forEach(item => {
+        if (item['地点'] && item['地点'].match(/周(\S)值班组长：\s+(\S+)\s+(\d{11})/)) {
+            const result = /周(\S)值班组长：\s+(\S+)\s+(\d{11})/.exec(item['地点']);
+            tempTable[days.indexOf(result[1])].content.push({
+                place: '值班组长',
+                worker: result[2],
+                phone: result[3]
+            });
+        }
+    });
 
-  // change data
-  data.tousu = tempTousu
-  data.table = tempTable
+    // handle tousu data
+    let tempTousu = [];
+    jsonSheet.forEach(item => {
+        for (let value of Object.values(item)) {
+            if (value.includes('投诉')) {
+                let temp = value.match(/\s+(\S+)\s+(\S+)/);
+                tempTousu.push({
+                    worker: temp[1].split(/-+/)[0],
+                    phone: temp[1].split(/-+/)[1]
+                });
+                tempTousu.push({
+                    worker: temp[2].split(/-+/)[0],
+                    phone: temp[2].split(/-+/)[1]
+                });
+                return;
+            }
+        }
+    });
+
+    // change data
+    data.tousu = tempTousu;
+    data.table = tempTable;
 }
 
 function getWorkList () {
-  return data
+    return data;
 }
 
-const workListModel = { start, refreshData, getWorkList }
+const workListModel = { start, refreshData, getWorkList };
 
-module.exports = workListModel
+module.exports = workListModel;
