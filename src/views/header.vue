8ba4fceb9c6193a92c97f72d53e39cd139cd2cd2
jay xie <592291560@qq.com>
2020-01-01 23:34:08 +0800

    Update header.vue
    update wiki&remove some resource unavailable

--- src/views/header.vue
+++ src/views/header.vue
@@ -23,16 +23,14 @@
             <a href="javascript:void(0);">关于我们</a>
             <ul class="header-nav--nesting">
               <li class="header-nav__item--nesting">
-                <a href="http://wiki.sola.love" target="_blank" rel="noopener">网维wiki</a>
+                <a href="https://wiki.zsxyww.com" target="_blank" rel="noopener">网维wiki</a>
               </li>
 
               <li class="header-nav__item--nesting">
                 <a href="https://baike.baidu.com/item/电子科技大学中山学院网络维护科" target="_blank" rel="noopener">百度百科</a>
               </li>
 
-              <li class="header-nav__item--nesting">
-                <a href="http://baike.sogou.com/v63143404.htm?fromTitle=电子科技大学中山学院网络维护科" target="_blank" rel="noopener">搜狗百科</a>
-              </li>
+              
             </ul>
           </li>
         </ul>
